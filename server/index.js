37e4f3e73dac56c97225f2924b68d72aa5406b91
Adamm Norgren <norgren.adamm@gmail.com>
2020-04-25 00:55:07 -0700

    heroku rnnable

--- server/index.js
+++ server/index.js
@@ -39,11 +39,11 @@ app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api/braintree', braintreeRoutes);
 app.use('/api/order', orderRoutes);
-//
-// app.use(express.static(path.join(__dirname, '..', 'build')));
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
-// });
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
